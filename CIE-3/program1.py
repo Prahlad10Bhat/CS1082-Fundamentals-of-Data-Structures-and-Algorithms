89b2fe4dcc74df32a45a1b6a42406a839041caa6
Prahlad10Bhat <prahlad10bhat@gmail.com>
2025-04-16 10:53:17 +0530

    Create program1.py

---
+++ CIE-3/program1.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, data):
+        self.data=data
+        self.next=None
+
+class Stack:
+    def empty():
+    
+    def push():
+
+    def pop():
+
+    def peek():
