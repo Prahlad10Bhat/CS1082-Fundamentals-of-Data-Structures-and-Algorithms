b237cfb99af7081fee79c04974aa15d3bf6fb741
Prahlad10Bhat <prahlad10bhat@gmail.com>
2025-04-16 10:29:52 +0530

    Create program 2.py

---
+++ CIE-3/program 2.py
@@ -0,0 +1,44 @@
+class Stack:
+    def empty():
+        if stack is empty:
+            return True
+        else:
+            return False
+
+
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def in_order_traversal(self, node):
+        if node is not None:
+            self.in_order_traversal(node.left)
+            print(node.data, end=" ")
+            self.in_order_traversal(node.right)
+
+    def post_order_traversal(self, node):
+        if node is not None:
+            self.post_order_traversal(node.left)
+            self.post_order_traversal(node.right)
+            print(node.data, end=" ")
+    
+tree = BinaryTree()
+tree.root = TreeNode(1)
+tree.root.left = TreeNode(2)
+tree.root.right = TreeNode(3)
+tree.root.left.left = TreeNode(4)
+tree.root.left.right = TreeNode(5)
+
+print("in order traversal:")
+tree.in_order_traversal(tree.root)
+print()  
+
+print("post order traversal:")
+tree.post_order_traversal(tree.root)
+print()  
